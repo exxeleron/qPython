8b5fb23158bbad2411fd5cecef1ee79c0099c33f
maciejlach <maciej.lach@gmail.com>
2014-06-04 14:25:59 +0200

    update project meta-information

--- setup.py
+++ setup.py
@@ -17,6 +17,7 @@
 from distutils.core import setup
 
 import numpy
+import os
 
 try:
     from Cython.Build import cythonize
@@ -25,26 +26,50 @@ except ImportError:
 else:
     use_cython = True
 
-
 if use_cython:
     ext_modules = cythonize('qpython/fastutils.pyx')
 else:
     ext_modules = []
 
 
+# Utility function to read the README file.
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()    
+
+
 setup(name = 'qPython',
-      version = '1.0 Beta 3',
+      version = '1.0 Beta 4',
       description = 'kdb+ interfacing library for Python',
+      long_description=read('README.rst'),
 
       author = 'exxeleron',
-      author_email = 'info@exxeleron.com',
+      author_email = 'kdbtools@devnet.de',
       url = 'https://github.com/exxeleron/qPython',
       license = 'Apache License Version 2.0',
 
       ext_modules = ext_modules,
       include_dirs = [numpy.get_include()],
 
+      keywords = ['kdb+'],
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Environment :: Console',
+          'Intended Audience :: Developers',
+          'Intended Audience :: Education',
+          'Intended Audience :: End Users/Desktop',
+          'Intended Audience :: Financial and Insurance Industry',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: Apache Software License',
+          'Operating System :: MacOS',
+          'Operating System :: Microsoft :: Windows',
+          'Operating System :: POSIX',
+          'Operating System :: Unix',
+          'Programming Language :: Python',
+          'Topic :: Database :: Front-Ends',
+          'Topic :: Scientific/Engineering',
+          'Topic :: Software Development',
+          ],
       packages = ['qpython', 'samples', ''],
-      package_data = {'': ['LICENSE', 'CHANGELOG.txt', 'README.md', 'requirements.txt']},
+      package_data = {'': ['LICENSE', 'CHANGELOG.txt', 'README.rst', 'requirements.txt']},
      )
 
